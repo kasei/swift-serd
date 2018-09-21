84b8d07d3d3a9af1bb51ed3bd1710b0c2f1a4e18
Gregory Todd Williams <greg@evilfunhouse.com>
2018-09-21 09:16:23 -0700

    Update Package.swift.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,19 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
 	name: "serd",
-	pkgConfig: "serd-0",
-	providers: [
-		.Brew("serd"),
-		.Apt("libserd-dev")
+	products: [
+		.library(name: "serd", targets: ["serd"])
+	],
+	targets: [
+		.systemLibrary(
+			name: "serd",
+			pkgConfig: "serd-0",
+			providers: [
+				.brew(["serd"]),
+				.apt(["libserd-dev"])
+			]
+		)
 	]
 )
